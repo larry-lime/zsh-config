80d0b847f316f0d72439d3111e684641be80f3c3
Lawrence Lim <ll4715@nyu.edu>
2022-06-23 23:14:36 +0800

    Added much more functionality to fzf.zsh
    Added f, c, g, and s functions
    f -> searches through select project directories for files and directories.
    Opens the selection in a vim buffer
    c -> searches the cwd and opens the file in an editor
    g -> uses ripgrep to search the cwd for a string and goes to the
    location of the string once selected.
    s -> searches select folders for a given directory and cds to it once
    selected

--- fzf.zsh
+++ fzf.zsh
@@ -3,44 +3,50 @@
 # export FZF_CTRL_T_COMMAND='t'
 
 function f(){
-  FILEPATH=$(find . | fzf --height 70% --layout=reverse --preview 'batcat --style=numbers --color=always --line-range :500 {}')
-  if [ -z "$FILEPATH" ]; then
+  FILEPATH=$(find ~/Programming ~/.config ~/Life ~/Career ~/Extracurriculars | fzf --height 100% --preview 'batcat --style=numbers --color=always --line-range :500 {}')
+
+  if [ -z $FILEPATH ]; then
     return
   fi
-  basename "$FILEPATH"
-  FILE="$(basename -- $FILEPATH)"
+
+  FILE=$(basename $FILEPATH)
   DIR_PATH=$(dirname $FILEPATH)
+  PARENT_DIR=$(basename $DIR_PATH)
   cd $DIR_PATH
   $EDITOR $FILE
 }
 
+function c() {
+  rg --files | fzf --preview 'batcat --style=numbers --color=always {} --line-range :500 {}'
+}
+
+function g() {
+        out=$(rg \
+        --column \
+        --line-number \
+        --no-column \
+        --no-heading \
+        --fixed-strings \
+        --ignore-case \
+        --hidden \
+        --follow \
+        --glob '!.git/*' "$1" \
+	| awk -F  ":" '/1/ {start = $2<40 ? 0 : $2 - 40; end = $2 + 40; print $1 " " $2 " " start ":" end}' \
+	| fzf --preview 'batcat --color always {1} --highlight-line {2} --line-range {3}')
+        
+  if [ -z "$out" ]; then
+    return
+  fi
+  read -r filename line <<< "${out}"
+  LINE_NUMBER=${line%% *}
+  $EDITOR -c ":$LINE_NUMBER" $filename
+}
+
 function s() {
-  cd ~
-  DIR_PATH=$(find . -type d | fzf --height 70% --layout=reverse --preview 'tree -C {}')
+  DIR_PATH=$(find ~/Programming ~/.config ~/Life ~/Career ~/Extracurriculars -type d | fzf --height 100% --preview 'tree -C {}')
   if [ -z "$DIR_PATH" ]; then
     return
-  else
-    basename "$DIR_PATH"
-    NAME="$(basename -- $DIR_PATH)"
-    cd $DIR_PATH
-    if [ -z "$TMUX" ]; then
-      tmux new -s $NAME
-    else
-      return
-    fi
   fi
-}
-
-function fd(){
-  cd $(find . -type d | fzf --height 70% --layout reverse --preview 'tree -C {}')
-}
-
-_fzf_comprun() {
-  local command=$1
-  shift
-
-  case "$command" in
-    cd)           fzf "$@" --preview 'tree -C {} | head -200' ;;
-    *)            fzf "$@" ;;
-  esac
+  NAME="$(basename -- $DIR_PATH)"
+  cd $DIR_PATH
 }
