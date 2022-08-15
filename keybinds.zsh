bb2124cc9ebbec669387d7ec0955f084530ad5dd
Lawrence Lim <ll4715@nyu.edu>
2022-08-15 15:39:26 +0800

    Added alt-a binding to trigger fzf autocomplete

--- keybinds.zsh
+++ keybinds.zsh
@@ -2,3 +2,4 @@ bindkey -s '\ef' 'tnav\n' # find files in select folders
 bindkey -s '\eg' 'tnav -r\n' # find files in select folders
 bindkey -s '^h' 'cd ~\n' # go home
 bindkey -s '\et' 'tmux\n' # launch tmux
+bindkey -s '\ea' '**	' # fzf autocomplee
