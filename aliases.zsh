10945d85888b47634417fe3907798b451d01873a
Lawrence Lim <ll4715@nyu.edu>
2022-09-05 17:16:37 +0800

    Updated

--- aliases.zsh
+++ aliases.zsh
@@ -7,23 +7,21 @@
 # -----------------------------------------------------------------
 # File Shortcuts
 # -----------------------------------------------------------------
-alias ics="cd /mnt/c/Users/larry/nyu/ics"
-alias fin="cd ~/Programming/projects/py_finance/"
-alias stats="cd /mnt/c/Users/larry/nyu/stats"
-alias wai="cd /mnt/c/Users/larry/nyu/WAI"
-alias life="cd ~/Life"
 alias home="cd /mnt/c/Users/larry"
 alias down='cd /mnt/c/Users/larry/Downloads'
 alias doc='cd /mnt/c/Users/larry/OneDrive/Documents'
 alias desk='cd /mnt/c/Users/larry/OneDrive/Desktop'
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias nvimrc='cd ~/.config/nvim'
+alias zshrc='cd ~/.config/zsh'
+alias tmuxrc='cd ~/.config/tmux'
+alias lbin='cd ~/.local/bin'
+alias nplug='cd ~/.local/share/nvim/site/pack/packer/start'
 
 # -----------------------------------------------------------------
 # Aliases to Launch Applications
 # -----------------------------------------------------------------
 alias word="winword.exe"
 alias explore="explorer.exe"
-alias ger="ranger"
 alias nv="nvim"
 alias tt="taskwarrior-tui"
 alias tsource="tmux source ~/.config/tmux/tmux.conf"
@@ -33,7 +31,6 @@ alias t="/usr/local/bin/tmux"
 alias tnav="source $HOME/.local/bin/tnav"
 alias lg="lazygit"
 alias chrome="/mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe"
-
 
 # -----------------------------------------------------------------
 # Run Files
@@ -60,6 +57,7 @@ alias ll="exa -l"
 alias tree="exa -T --icons"
 alias hhstart="~/.local/bin/hardhat-boilerplate"
 alias genr="~/.local/bin/generate-README"
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 #alias cd="z"
 
 # -----------------------------------------------------------------
