d0f0752d34c3e52f1b11a76fe6ba59afd46e22d0
Lawrence Lim <ll4715@nyu.edu>
2022-11-15 09:54:50 +0800

    Remaped fzf autocomplete

--- keybinds.zsh
+++ keybinds.zsh
@@ -5,4 +5,4 @@ bindkey -s '\eg' 'tnav -r\n' # find files in select folders
 bindkey -s '^h' 'cd ~\n' # go home
 bindkey -s '^_' 'cd /\n' # go home
 bindkey -s '\et' 'tmux\n' # launch tmux
-bindkey -s '\ea' '**	' # fzf autocomplee
+bindkey -s '^a' '**	' # fzf autocomplee
