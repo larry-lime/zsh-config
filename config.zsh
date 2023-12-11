c7b656dc346fd8fef92eb4d1b335ffb631d4cdf1
Lawrence Lim <ll4715@nyu.edu>
2023-12-11 16:26:16 -0500

    update config

--- config.zsh
+++ config.zsh
@@ -5,16 +5,28 @@
 #  \____\___/|_| |_|_| |_|\__, |
 #                         |___/
 
+# --------------
+# PATH VARIABLES
+# --------------
+
 # Export foundry path
 export PATH="$PATH:/Users/lawrencelim/.foundry/bin" # Foundry Path
 export PATH="/Users/lawrencelim/.local/bin:$PATH" # Poetry Path
 export PATH="$PATH:/Users/lawrencelim/go/bin" # Go Path
+
+# -----------
+# GLOBAL VARS
+# -----------
 
 export EDITOR="nvim"
 export BROWSER="Brave.app"
 export DOCKER_HOST="unix://$HOME/.colima/docker.sock" # Colima Docker Host
 export GCLI=false # Global state var to lazy load Github Copilot CLI
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# -----
+# EVALS
+# -----
 
+eval "$(/opt/homebrew/bin/brew shellenv)" # Homebrew Path
 eval "$(starship init zsh)" # Starship Prompt
+eval "$(zoxide init zsh)" # Zoxide
