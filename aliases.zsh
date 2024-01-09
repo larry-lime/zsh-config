f30f237e8c8eec0f0698a8c3a1d7b6be7fa71444
Lawrence Lim <you@example.com>
2024-01-09 14:11:44 -0500

    update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,51 +1,58 @@
-#    ___    ___
-#    /   |  / (_)___ _________  _____
-#   / /| | / / / __ `/ ___/ _ \/ ___/
-#  / ___ |/ / / /_/ (__  )  __(__  )
-# /_/  |_/_/_/\__,_/____/\___/____/
+#     _    _ _
+#    / \  | (_) __ _ ___  ___  ___
+#   / _ \ | | |/ _` / __|/ _ \/ __|
+#  / ___ \| | | (_| \__ \  __/\__ \
+# /_/   \_\_|_|\__,_|___/\___||___/
 
-# -----------------------------------------------------------------
-# File Shortcuts
-# -----------------------------------------------------------------
-alias down="cd $HOME/Downloads"
-alias doc="cd $HOME/Documents"
+# -----------------------
+# REMAPPED SHELL COMMANDS
+# -----------------------
 
-# -----------------------------------------------------------------
-# Aliases to Launch Applications
-# -----------------------------------------------------------------
-alias o="open"
-alias nv="nvim"
-alias tsource="tmux source ~/.config/tmux/tmux.conf"
-alias tconf="cd ~/.config/tmux/ && nvim tmux.conf"
-alias t="tmux"
-alias st="$HOME/.dotfiles/.local/bin/start_tmux"
-alias start-tmux="$HOME/.dotfiles/.local/bin/start_tmux"
-alias tnav="source $HOME/.local/bin/tnav"
-alias lg="lazygit"
-alias hg="hugo"
-alias of="open . -a finder"
-alias dw="open ~/Downloads/ -a finder<CR>"
-alias ger="ranger"
-
-# -----------------------------------------------------------------
-# Run Files
-alias py="python3"
-alias ipython="python3 -m IPython"
-
-# -----------------------------------------------------------------
-# Command Aliases
-# -----------------------------------------------------------------
-alias cl="clear"
-alias cf="cfile"
-alias so="source ~/.config/zsh/.zshrc"
-alias md="mkdir"
-alias rd="rmdir"
-alias myip='curl http://ipecho.net/plain; echo'
-alias distro='cat /etc/*-release'
 alias ls="exa"
 alias ll="exa -l -snew"
 alias la="exa -a"
 alias tree="exa -T --icons"
-alias \?\?="what-the-shell"
-alias git\?="git-assist"
-alias gh\?="gh-assist"
+alias cat="bat"
+# alias cd="z" # zoxide
+
+# ------------------------
+# SHORTENED SHELL COMMANDS
+# ------------------------
+
+alias o="open"
+alias md="mkdir"
+alias rd="rmdir"
+alias cl="clear"
+
+# --------------
+# DIRECTORY SHORTCUTS
+# --------------
+
+alias down="cd $HOME/Downloads"
+alias doc="cd $HOME/Documents"
+
+# --------------------
+# RUN AND SOURCE FILES
+# --------------------
+
+alias tsource="tmux source ~/.config/tmux/tmux.conf"
+alias tnav="source $HOME/.local/bin/tnav"
+alias sep="/Users/lawrencelim/Scripts/generate-separator.sh"
+alias so="source ~/.config/zsh/.zshrc"
+alias \#="cw ai"
+
+# ------------------
+# SHORTENED COMMANDS
+# ------------------
+
+alias nv="nvim"
+alias t="tmux"
+alias hg="hugo"
+alias lg="lazygit"
+alias py="python3"
+alias ipython="python3 -m IPython"
+alias code="code-insiders"
+
+alias \?\?="gh copilot suggest -t shell "
+alias git\?="gh copilot suggest -t git"
+alias gh\?="gh copilot suggest -t gh"
