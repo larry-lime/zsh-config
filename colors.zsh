9bc83489149a721709b52733d93c3330b2d70a19
Lawrence Lim <you@example.com>
2024-01-09 14:12:54 -0500

    added colors.zsh

---
+++ colors.zsh
@@ -0,0 +1,25 @@
+#   ____      _
+#  / ___|___ | | ___  _ __ ___
+# | |   / _ \| |/ _ \| '__/ __|
+# | |__| (_) | | (_) | |  \__ \
+#  \____\___/|_|\___/|_|  |___/
+
+# Macchiato (Dark)
+# export FZF_DEFAULT_OPTS=" \
+# --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+# --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+# --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
+
+# Latte (Light)
+# export FZF_DEFAULT_OPTS=" \
+    # --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+    # --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+    # --color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39"
+
+# Tokyo Night (Dark)
+# export FZF_DEFAULT_OPTS="
+#   --color=fg:#c0caf5,bg:#1a1b26,hl:#bb9af7
+#   --color=fg+:#c0caf5,bg+:#1a1b26,hl+:#7dcfff
+#   --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff
+#   --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a
+#   "
